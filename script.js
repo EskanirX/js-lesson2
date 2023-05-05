7bdc78cc84396c2116dd2856af950348e09eb78c
EskanirX <125078699+EskanirX@users.noreply.github.com>
2023-05-06 00:48:25 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,100 @@
+// 5 დან 100 მდე
+
+// let x = 5
+// while(x < 100){
+//     console.log(x);
+//     x++;
+// }
+
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 12, 18];
+// for (let x = 0; x < array1.length; x++) {
+//     if(array1[x] > 0){
+//         if(array1[x] < 10)
+//         console.log(array1[x])
+//     }
+// }
+
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (let x = 0; x < array2.length; x++) {
+//     if (array2[x] == 5){
+//         console.log('არის');
+//     }
+// }
+
+// let array3 = [1, 2, 3, 4, 5]
+// let sum = 0
+//     for (let x of array3) {
+//         sum += x;
+//     }
+//     console.log(sum)
+
+// let array4 = [1, 2, 3, 7, 6, 9];
+// let sum = 0
+//     for ( let x of array4) {
+//         sum += x
+//     }
+//     console.log(sum/6);
+
+// let array5 = [1, 2, 3, 7, 6, 9];
+// for (let x = 0; x < array5.length; x++) {
+//   if (array5[x] == 7) {
+//     continue;
+//   }
+//   console.log(array5[x]);
+// }
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+// console.log(user.studentstatus);
+
+// let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
+// if (user.age < 18){
+//     if(user.studentstatus == 'active')
+//     console.log('hello');
+//     if (user.name == 'levan'){
+//         console.log('hello levan')
+//     }
+//     if (user.studentstatus == 'active'){
+//         if (user.age < 25)
+//         console.log('hello world')
+//     }
+//     else ('error')
+
+// }
+
+// let array = [
+//     [2, -3, 5, 10],
+//     [25, -24, -11, 100],
+//     [-6, -7, 10],
+//   ];
+
+// for (let x = 0; x < array.length; x++ ){
+//     // console.log(array[x]);
+//     for (let y = 0; y < array[x].length; y++){
+//         // console.log(array[x][y]);
+//         if (array[x][y] > 0){
+//             console.log(array[x][y])
+//         }
+//     }
+// }
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+// for (let x = 0; x < array.length; x++){
+//     if(array[x] % 2 == 0)
+//     console.log(array[x] + "luwi");
+// }
+// for (let x = 0; x < array.length; x++){
+//     if(array[x] % 2 == 1)
+//     console.log(array[x] + "kenti");
+// }
+
